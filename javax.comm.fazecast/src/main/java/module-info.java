ee1f17733d48e7848f30eccfceeb670be37d0848
chav1961 <chav1961@winmain>
2021-06-24 22:46:23 +0500

    DAily commit

--- javax.comm.fazecast/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module javax.comm.fazecast {
-	requires transitive javax.comm.basic;
-	uses javax.comm.CommDriver;
-	provides javax.comm.CommDriver with
-		javax.comm.fazecast.FazecastCommDriver;
-}
